41857e57a024520eb326164805b88c6f2595b3b5
Thu23130317 <23130317@st.hcmuaf.edu.vn>
2025-11-11 00:23:09 +0700

    xem chi tiet don hang

--- admin/admin.js
+++ admin/admin.js
@@ -1,34 +1,58 @@
 document.addEventListener("DOMContentLoaded", () => {
     const modal = document.getElementById('customer-modal');
-    const dongNut = modal.querySelector('.close-modal');
-    const moNutXem = document.querySelectorAll('.customer-table .btn-view');
+    const customerViewBtn = document.querySelectorAll('.customer-table .btn-view');
+
+    if (modal) {
+        const  customerClose = modal.querySelector('.close-modal');
+        customerViewBtn.forEach(nut => {
+            nut.addEventListener('click', () => {
+                modal.style.display = 'flex';
+            });
+        });
+
+        customerClose.addEventListener('click', () => {
+            modal.style.display = 'none';
+        });
+
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal) {
+                modal.style.display = 'none';
+            }
+        });
+    }
+
+    //order
+    const order = document.getElementById('order-modal');
+    const orderViewBtn = document.querySelectorAll('.order-table .btn-view');
+    if (order) {
+        const orderClose = order.querySelector('.close-modal');
+        orderViewBtn.forEach(nut => {
+            nut.addEventListener('click', () => {
+                order.style.display = 'flex';
+            });
+        });
+        orderClose.addEventListener('click', () => {
+            order.style.display = 'none';
+            });
+
+        order.addEventListener('click', (e) => {
+            if (e.target === order) {
+                order.style.display = 'none';
+            }
+        });
+    }
     const tabLinks = document.querySelectorAll(".tab-link");
     const tabContents = document.querySelectorAll(".tab-content");
+    if (tabLinks) {
+        tabLinks.forEach(link => {
+            link.addEventListener("click", () => {
+                tabLinks.forEach(btn => btn.classList.remove("active"));
 
-    moNutXem.forEach(nut => {
-        nut.addEventListener('click', () => {
-            modal.style.display = 'flex';
+                tabContents.forEach(tab => tab.classList.remove("active"));
+
+                link.classList.add("active");
+                document.getElementById(link.dataset.tab).classList.add("active");
+            });
         });
-    });
-
-    dongNut.addEventListener('click', () => {
-        modal.style.display = 'none';
-    });
-
-    modal.addEventListener('click', (e) => {
-        if (e.target === modal) {
-            modal.style.display = 'none';
-        }
-    });
-
-    tabLinks.forEach(link => {
-        link.addEventListener("click", () => {
-            tabLinks.forEach(btn => btn.classList.remove("active"));
-
-            tabContents.forEach(tab => tab.classList.remove("active"));
-
-            link.classList.add("active");
-            document.getElementById(link.dataset.tab).classList.add("active");
-        });
-    });
+    }
 });
