27120b54fcb2a4403d9c7a3d0962a03df7a28b8c
LaiThiHoa01 <23130111@st.hcmuaf.edu.vn>
2025-12-09 23:07:05 +0700

    Dịch vụ Cloudinary và tải danh sách sản phẩm bằng database

---
+++ src/main/java/services/ProductService.java
@@ -0,0 +1,17 @@
+package services;
+
+import dao.ProductDao;
+import model.Product;
+
+import java.util.List;
+
+public class ProductService {
+    ProductDao productDao = new ProductDao();
+    public List<Product> getListProduct() {
+        return productDao.getListProduct();
+    }
+
+    public Product getProduct(int id) {
+        return  productDao.getProduct(id);
+    }
+}
