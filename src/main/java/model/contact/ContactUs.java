a749be4f441ee23b4474a330b7a092205f8b3805
LaiThiHoa01 <23130111@st.hcmuaf.edu.vn>
2025-12-27 21:18:49 +0700

    page Contact admin

--- src/main/java/model/contact/ContactUs.java
+++ src/main/java/model/contact/ContactUs.java
@@ -1,10 +1,13 @@
 package model.contact;
 
+import model.AId;
 import org.jdbi.v3.core.mapper.reflect.ColumnName;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-public class ContactUs {
+public class ContactUs extends AId implements Serializable {
     @ColumnName("full_name")
     private String fullName;
     private String email;
@@ -15,12 +18,21 @@ public class ContactUs {
     @ColumnName("status_message")
     private String statusMessage;
 
-    public ContactUs(String fullName, String email, String messageBody) {
+    public ContactUs( String fullName, String email, String messageBody) {
         this.fullName = fullName;
         this.email = email;
         this.messageBody = messageBody;
 
     }
+    public ContactUs(int id, String fullName, String email, String messageBody, LocalDateTime receivedAt, String statusMessage) {
+        this.setId(id); // Gọi hàm setter từ lớp cha
+        this.fullName = fullName;
+        this.email = email;
+        this.messageBody = messageBody;
+        this.receivedAt = receivedAt;
+        this.statusMessage = statusMessage;
+    }
+
 
     public ContactUs() {
     }
@@ -47,5 +59,26 @@ public class ContactUs {
 
     public void setMessageBody(String messageBody) {
         this.messageBody = messageBody;
+    }
+
+    public String getFormattedDate() {
+        if (this.receivedAt == null) return "";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        return this.receivedAt.format(formatter);
+    }
+
+    public void setReceivedAt(LocalDateTime receivedAt) {
+        this.receivedAt = receivedAt;
+    }
+    public LocalDateTime getReceivedAt() {
+        return receivedAt;
+    }
+
+    public String getStatusMessage() {
+        return statusMessage;
+    }
+
+    public void setStatusMessage(String statusMessage) {
+        this.statusMessage = statusMessage;
     }
 }
